9f790ae1b6c98e0f82ce044f00be8f25a39df1c9
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-04-02 11:35:29 +0200

    Update runOnTrue.js

--- src/generic/runOnTrue.js
+++ src/generic/runOnTrue.js
@@ -3,7 +3,7 @@
  * @param {() => boolean} conditionCallback A function that **must** return a boolean, if it returns true it runs the callback
  * @param {() => void} callback Runs when condition is met 
  * @param {number} refreshRateInMs Amount of time between checks
- * @example runOnTrue(() => World.isLoaded(), runMeWhenWorldLoads, 1000)
+ * @example runOnTrue(World.isLoaded, runMeWhenWorldLoads, 1000)
  */
 function runOnTrue(conditionCallback, callback, refreshRateInMs) {
     new Thread(() => {
