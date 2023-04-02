909c1b298f3b162d17ccd854645efe1a5da12935
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-04-02 11:38:33 +0200

    Fix example.js

--- example.js
+++ example.js
@@ -10,4 +10,4 @@ function runOnWorldLoad() {
     ChatLib.chat(Coffee.pickRandom(Messages)) // Pseudorandom message from array
 }
 
-Coffee.runOnTrue(World.isLoaded(), runOnWorldLoad(), 1000) // Check if world is loaded every 1000ms (1s) and if it is run the function
+Coffee.runOnTrue(World.isLoaded, runOnWorldLoad, 1000) // Check if world is loaded every 1000ms (1s) and if it is run the function
